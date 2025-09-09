e00a565f38f691db7b4ba25869a58b1c55dfd280
hyewon4052 <s2311@e-mirim.hs.kr>
2025-09-09 16:35:26 +0900

    할일 표시하는 svg 추가 ( 색상은 임시색상으로 설정 )

---
+++ src/components/TodoIconSvg.tsx
@@ -0,0 +1,42 @@
+const TodoIconSvg = () => {
+  return (
+    <svg
+      width="21"
+      height="21"
+      viewBox="0 0 21 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle
+        cx="6.46154"
+        cy="6.46154"
+        r="6.46154"
+        fill={"#222222"}
+        fillOpacity={"0.9"}
+      />
+      <circle
+        cx="6.46154"
+        cy="14.5387"
+        r="6.46154"
+        fill={"#222222"}
+        fillOpacity={"0.9"}
+      />
+      <circle
+        cx="14.5387"
+        cy="14.5387"
+        r="6.46154"
+        fill={"#222222"}
+        fillOpacity={"0.9"}
+      />
+      <circle
+        cx="14.5387"
+        cy="6.46154"
+        r="6.46154"
+        fill={"#222222"}
+        fillOpacity={"0.9"}
+      />
+    </svg>
+  );
+};
+
+export default TodoIconSvg;
