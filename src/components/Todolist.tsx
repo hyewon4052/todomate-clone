ef9b9b076390c54bbf332a389857cad425470577
hyewon4052 <s2311@e-mirim.hs.kr>
2025-09-09 16:44:30 +0900

    할일 별 더보기 버튼 클릭시 모달 뜨도록 구현 ( modal-sheet )

--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,9 +1,10 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import { Ellipsis, Earth, Plus } from "lucide-react";
+import { Ellipsis, Earth, Plus, SquarePen, Trash } from "lucide-react";
 import { atom, useAtom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 import TodoIcon from "./TodoIconSvg";
+import { Sheet } from "react-modal-sheet";
 
 export type Todo = {
   id: number;
@@ -14,11 +15,13 @@ export type Todo = {
 const inputValueAtom = atom("");
 const todoListAtom = atomWithStorage<Todo[]>("todoList", []);
 const todoOpenBtnAtom = atom(false);
+const modalOpenBtnAtom = atom(false);
 
 const TodoList = () => {
   const [inputValue, setInputValue] = useAtom(inputValueAtom);
   const [todoList, setTodoList] = useAtom(todoListAtom);
   const [todoOpen, setTodoOpen] = useAtom(todoOpenBtnAtom);
+  const [modalOpen, setModalOpen] = useAtom(modalOpenBtnAtom);
 
   function onChangeValue(e: React.ChangeEvent<HTMLInputElement>) {
     setInputValue(e.target.value);
@@ -34,6 +37,11 @@ const TodoList = () => {
       setTodoList([...todoList, newTodo]);
       setInputValue("");
     }
+  }
+
+  function deleteItem(id: number) {
+    const updateTodoList = todoList.filter((todo) => todo.id !== id);
+    setTodoList(updateTodoList);
   }
 
   return (
@@ -80,13 +88,73 @@ const TodoList = () => {
           <div css={TodoItemRow} key={todo.id}>
             <TodoIcon />
             {todo.text}
-            <Ellipsis cursor={"pointer"} />
+            <Ellipsis cursor={"pointer"} onClick={() => setModalOpen(true)} />
+            <Sheet isOpen={modalOpen} onClose={() => setModalOpen(false)}>
+              <Sheet.Container
+                style={{
+                  left: "25%",
+                  transform: "translate(-50%, -50%)",
+                  width: "50%",
+                  height: "100%",
+                  backgroundColor: "#222222",
+                }}
+              >
+                <Sheet.Header />
+                <Sheet.Content>
+                  <div css={ModalBox}>
+                    <span>{todo.text}</span>
+                    <div css={ModalItemBox}>
+                      <div css={ModalItem}>
+                        <SquarePen />
+                        Edit
+                      </div>
+                      <div css={ModalItem} onClick={() => deleteItem(todo.id)}>
+                        <Trash />
+                        Delete
+                      </div>
+                    </div>
+                  </div>
+                </Sheet.Content>
+              </Sheet.Container>
+              <Sheet.Backdrop />
+            </Sheet>
           </div>
         ))}
       </div>
     </div>
   );
 };
+
+const ModalItem = css`
+  display: flex;
+  flex-direction: column;
+  width: 230px;
+  height: 100px;
+  background-color: #333333;
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  cursor: pointer;
+`;
+
+const ModalItemBox = css`
+  margin-top: 20px;
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+`;
+
+const ModalBox = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  span {
+    font-weight: 800;
+  }
+`;
 
 const TodoItemRow = css`
   display: flex;
