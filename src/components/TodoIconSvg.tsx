47dcc17dc50606c7526841b39326db0b3335e1c2
hyewon4052 <s2311@e-mirim.hs.kr>
2025-09-11 11:16:22 +0900

    #5 캘린더 내 svg 색상 위치 설정

--- src/components/TodoIconSvg.tsx
+++ src/components/TodoIconSvg.tsx
@@ -1,8 +1,35 @@
 type TodoIconSvgProps = {
-  color?: string;
+  colors?: string[];
 };
 
-const TodoIconSvg = ({ color = "#222222" }: TodoIconSvgProps) => {
+const TodoIconSvg = ({ colors = [] }: TodoIconSvgProps) => {
+  const defaultColor = "var(--main-gray)";
+  let circleColors: string[] = Array(4).fill(defaultColor);
+
+  switch (colors.length) {
+    case 1:
+      circleColors = Array(4).fill(colors[0]);
+      break;
+    case 2:
+      circleColors[0] = colors[1];
+      circleColors[1] = colors[0];
+      circleColors[2] = colors[0];
+      circleColors[3] = colors[1];
+      break;
+    case 3:
+      circleColors[0] = colors[1];
+      circleColors[1] = colors[1];
+      circleColors[2] = colors[2];
+      circleColors[3] = colors[0];
+      break;
+    case 4:
+      circleColors[0] = colors[2];
+      circleColors[1] = colors[0];
+      circleColors[2] = colors[3];
+      circleColors[3] = colors[1];
+      break;
+  }
+
   return (
     <svg
       width="21"
@@ -15,28 +42,28 @@ const TodoIconSvg = ({ color = "#222222" }: TodoIconSvgProps) => {
         cx="6.46154"
         cy="6.46154"
         r="6.46154"
-        fill={color}
+        fill={circleColors[0]}
         fillOpacity="0.9"
       />
       <circle
         cx="6.46154"
         cy="14.5387"
         r="6.46154"
-        fill={color}
+        fill={circleColors[1]}
         fillOpacity="0.9"
       />
       <circle
         cx="14.5387"
         cy="14.5387"
         r="6.46154"
-        fill={color}
+        fill={circleColors[2]}
         fillOpacity="0.9"
       />
       <circle
         cx="14.5387"
         cy="6.46154"
         r="6.46154"
-        fill={color}
+        fill={circleColors[3]}
         fillOpacity="0.9"
       />
     </svg>
